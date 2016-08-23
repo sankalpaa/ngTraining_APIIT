2c39fd93db2f824651be0576408512be61ac3b1c
sankalpa <absankalpa@gmail.com>
2016-08-23 10:55:46 +0530

    Initial Commit

---
+++ APIIT/app.js
@@ -0,0 +1,29 @@
+﻿var app = angular.module('addressBook', []);
+
+app.controller('createAddressBook', function($scope, addressBookService) {
+    $scope.title = 'Address Book';
+    $scope.save = function(firstName, lastName, email) {
+        addressBookService.saveAddress(firstName, lastName, email);
+    };
+});
+
+app.controller('searchAddress', function (addressBookService, $scope) {
+    $scope.addressBook= addressBookService.getAddresses();
+});
+
+app.service('addressBookService', function() {
+    var addressBook = [];
+    return {
+        saveAddress : function(firstName, lastName, email) {
+            addressBook.push({
+                firstName: firstName,
+                lastName: lastName,
+                email: email
+            });
+        },
+        getAddresses : function() {
+            return addressBook;
+        }
+    }
+
+});
